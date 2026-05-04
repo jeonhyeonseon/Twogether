8ed513c64b4d31aca9a288a28417dee051f53db7
jeonhyeonseon <good_pretty@naver.com>
2026-05-04 16:17:06 +0900

    refactor: 거래 유형 한글 추가 및 등록, 수정 페이지 수정 #49

--- src/main/java/com/think_different/think_different/transaction/domain/TransactionType.java
+++ src/main/java/com/think_different/think_different/transaction/domain/TransactionType.java
@@ -5,7 +5,12 @@ import lombok.Getter;
 @Getter
 public enum TransactionType {
 
-    INCOME,
-    EXPENSE
+    INCOME("수입"),
+    EXPENSE("지출");
 
+    private final String displayName;
+
+    TransactionType(String displayName) {
+        this.displayName = displayName;
+    }
 }
