af338df6a13167a0bf3491c71bb755808c94038e
jeonhyeonseon <good_pretty@naver.com>
2026-05-12 15:49:29 +0900

    feat: couple 엔터티 생성 #67

---
+++ src/main/java/com/think_different/think_different/couple/domain/Couple.java
@@ -0,0 +1,28 @@
+package com.think_different.think_different.couple.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "tbl_couple")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Couple {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime stareDate;
+
+    private LocalDateTime createdAt;
+
+    @Enumerated(EnumType.STRING)
+    private CoupleStatus coupleStatus;
+}
