08fe89440bc8c7db93184a2e24ad321a34b49b4f
jeonhyeonseon <good_pretty@naver.com>
2026-05-12 16:38:00 +0900

    feat: 초대 코드 발송할 수 있는 기능 추가 #70

--- src/main/java/com/think_different/think_different/couple/controller/CoupleController.java
+++ src/main/java/com/think_different/think_different/couple/controller/CoupleController.java
@@ -1,13 +1,14 @@
 package com.think_different.think_different.couple.controller;
 
 import com.think_different.think_different.config.webSecurity.CustomUserDetails;
-import com.think_different.think_different.couple.dto.CoupleStatusResponseDto;
 import com.think_different.think_different.couple.servicce.CoupleService;
 import com.think_different.think_different.member.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -27,6 +28,12 @@ public class CoupleController {
         if (connected) {
             return "couple/dashboard";
         }
+
+        return "couple/connect";
+    }
+
+    @GetMapping("/connect")
+    public String connectPage() {
 
         return "couple/connect";
     }
