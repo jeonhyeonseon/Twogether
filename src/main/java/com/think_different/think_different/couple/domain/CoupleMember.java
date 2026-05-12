afd11ce6818301d79248bc8cfb7f1541aace95cc
jeonhyeonseon <good_pretty@naver.com>
2026-05-12 15:50:02 +0900

    feat: coupleMember 엔터티 생성 #67

---
+++ src/main/java/com/think_different/think_different/couple/domain/CoupleMember.java
@@ -0,0 +1,31 @@
+package com.think_different.think_different.couple.domain;
+
+import com.think_different.think_different.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tbl_couple_member")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CoupleMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "couple_id")
+    private Couple couple;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime joinedAt; // 연결 날짜
+}
