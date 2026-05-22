337aa4c61854018fe643cb5eff6d24f58e4964d2
jeonhyeonseon <good_pretty@naver.com>
2026-05-22 10:22:45 +0900

    feat: 상대와 연결되었을 경우 #84

--- src/main/java/com/think_different/think_different/main/controller/MainController.java
+++ src/main/java/com/think_different/think_different/main/controller/MainController.java
@@ -1,15 +1,37 @@
 package com.think_different.think_different.main.controller;
 
+import com.think_different.think_different.config.webSecurity.CustomUserDetails;
+import com.think_different.think_different.couple.servicce.CoupleService;
+import com.think_different.think_different.member.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/main")
+@RequiredArgsConstructor
 public class MainController {
 
+    private final CoupleService coupleService;
+
     @GetMapping
-    public String main() {
+    public String main(@AuthenticationPrincipal CustomUserDetails customUserDetails,
+                       Model model) {
+
+        Member member = customUserDetails.getMember();
+
+        boolean connected = coupleService.isConnected(member);
+
+        model.addAttribute("connected", connected);
+        model.addAttribute("member", member);
+
+        if (connected) {
+            Member partner = coupleService.findPartner(member);
+            model.addAttribute("partner", partner);
+        }
         return "main/main";
     }
 
