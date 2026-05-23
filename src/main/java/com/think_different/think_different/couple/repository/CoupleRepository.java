ee39ea277e73f5856bcc6a5b653143e26b5c4340
jeonhyeonseon <good_pretty@naver.com>
2026-05-23 23:56:22 +0900

    refactor: 잘못된 메소드로 인한 오류 해결 #69

--- src/main/java/com/think_different/think_different/couple/repository/CoupleRepository.java
+++ src/main/java/com/think_different/think_different/couple/repository/CoupleRepository.java
@@ -1,12 +1,10 @@
 package com.think_different.think_different.couple.repository;
 
 import com.think_different.think_different.couple.domain.Couple;
-import com.think_different.think_different.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CoupleRepository extends JpaRepository<Couple, Long> {
 
-    Object findByMember(Member member);
 }
