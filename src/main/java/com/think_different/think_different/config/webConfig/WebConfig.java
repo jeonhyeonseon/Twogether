e61cd3a635f22c5c877826c2921c5e5e28b7aad8
jeonhyeonseon <good_pretty@naver.com>
2026-06-09 19:08:22 +0900

    config: 업로드 파일 및 접근 및 multipart 용량 설정 #100

---
+++ src/main/java/com/think_different/think_different/config/webConfig/WebConfig.java
@@ -0,0 +1,18 @@
+package com.think_different.think_different.config.webConfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations(
+                        "file:" + System.getProperty("user.dir") + "/uploads/"
+                );
+    }
+    
+}
