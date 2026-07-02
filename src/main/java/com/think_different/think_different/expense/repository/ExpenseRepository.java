91d6fd486b73563833d72097b920c1a093d6f476
jeonhyeonseon <good_pretty@naver.com>
2026-07-02 10:21:08 +0900

    refactor: 날짜 검색 조건 기능 수정 #108

--- src/main/java/com/think_different/think_different/expense/repository/ExpenseRepository.java
+++ src/main/java/com/think_different/think_different/expense/repository/ExpenseRepository.java
@@ -4,6 +4,8 @@ import com.think_different.think_different.couple.domain.Couple;
 import com.think_different.think_different.expense.domain.Expense;
 import com.think_different.think_different.expense.domain.ExpenseCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -16,4 +18,10 @@ public interface ExpenseRepository extends JpaRepository<Expense, Long> {
     List<Expense> findByCoupleAndCategoryAndExpenseDateBetweenOrderByExpenseDateDesc(Couple couple, ExpenseCategory expenseCategory, LocalDate startDate, LocalDate endDate);
 
     Optional<Expense> findByIdAndCoupleId(Long expenseId, Long id);
+
+    @Query("select min(e.expenseDate) from Expense e where e.couple = :couple")
+    Optional<LocalDate> findMinExpenseDateByCouple(@Param("couple") Couple couple);
+
+    @Query("select max(e.expenseDate) from Expense e where e.couple = :couple")
+    Optional<LocalDate> findMaxExpenseDateByCouple(@Param("couple") Couple couple);
 }
